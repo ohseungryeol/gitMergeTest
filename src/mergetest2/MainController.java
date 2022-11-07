2ee4ba1de1c2f7bd1bd47c11f732a48f127c2397
ohseungryeol <rollester@naver.com>
2022-11-07 16:32:30 +0900

    ds

--- src/mergetest2/MainController.java
+++ src/mergetest2/MainController.java
@@ -4,7 +4,7 @@ public class MainController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sdsdss");
+		System.out.println("sdsdsd");
 	}
 
 }
